20a50d52b66f4d88a9b7505a1787f44485cdcfab
umayer10-AI <mdumayer10@gmail.com>
2026-05-21 15:14:10 +0600

    add

---
+++ src/component/Action.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Action = () => {
+    return (
+        <div>
+            <button className='text-white bg-cyan-700 py-2 px-4 rounded-2xl'>Add</button>
+        </div>
+    );
+};
+
+export default Action;
