45ba970bf17a4b5fc85171a17e866f9c9f132b0a
umayer10-AI <mdumayer10@gmail.com>
2026-05-21 20:03:48 +0600

    add

--- src/component/Display.jsx
+++ src/component/Display.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Display = () => {
     return (
-        <div>
+        <div className='ml-5 mt-5'>
             <h2>User Data:</h2>
             <input type="text" placeholder='Enter name' className='border'/>
             <button className='py-1 px-3 bg-cyan-700 rounded-2xl m-4'>Add</button>
             <h2 className='mt-10'>User Data:</h2>
             <div>
-                
+
             </div>
         </div>
     );
--- src/redux/slice.js
+++ src/redux/slice.js
@@ -3,7 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 const counterSlice = createSlice({
     name: "counter",
     initialState:{
-        value:0
+        value:0,
+        users:[],
     },
     reducers:{
         increment: (v) => {
@@ -12,6 +13,9 @@ const counterSlice = createSlice({
         decrement: (v) => {
             v.value -= 1
         },
+        addUser: (v,acttion) => {
+            
+        }
     }
 })
 
